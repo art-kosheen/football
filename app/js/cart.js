0e43e2df44a7fe95edd7582a066760703791000f
art-kosheen <kashirin1242@gmail.com>
2016-12-07 20:01:56 +0200

    fix add product

--- app/js/cart.js
+++ app/js/cart.js
@@ -5,38 +5,47 @@
 // string = JSON.stringify(object)
 $(document).ready(function () {
 
-    var cart = {};
-
-    var temp_product_name = $(".tabs__controls-item.active .tabs__control-link").attr("data-name");
-    cart["data-name"]=temp_product_name;
+    var tempCart = {};
 
     $(".color__btn").on('click', (function (e) {
         e.preventDefault();
         $(this).addClass("active").siblings().removeClass("active");
     }));
 
-    $(".size__btn").click(function () {
+    $(".size__btn").on("click", (function (e) {
+        e.preventDefault();
 
         var temp_product_name = $(".tabs__controls-item.active .tabs__control-link").attr("data-name");
-        cart["data-name"]=temp_product_name;
+        tempCart["data-name"] = temp_product_name;
 
         var temp_product_size = $(".tabs__item.active .content__item__size input[name=\"size\"]:checked").val();
-        if (temp_product_size != undefined)
-            cart["data-size"]=temp_product_size;
+        if (temp_product_size != undefined) {
+            tempCart["data-size"] = temp_product_size;
+        }
 
         var temp_product_color = $(".color__btn.active").attr("data-color");
-        if (temp_product_color != undefined)
-            cart["data-color"]=temp_product_color;
+        if (temp_product_color != undefined) {
+            tempCart["data-color"] = temp_product_color;
+        }
 
-
-        if (cart["data-size"] == undefined || cart["data-color"] == undefined) {
+        if (tempCart["data-size"] == undefined || tempCart["data-color"] == undefined) {
             alert("выберите цвет/размер");
         }
         else {
+            var cart = tempCart;
+            tempCart = {};
             console.log(cart);
+            $(".tabs__item.active .content__item__size input[name=\"size\"]:checked").each(function () {
+                $(this).prop('checked', false);
+            });
+            $(".color__btn").removeClass("active");
+            //
+
+
+            $(".size__btn").text("Добавлено!");
             localStorage.setItem('itemsCart', JSON.stringify(cart));
         }
-    });
+    }));
 
-})
+});
 
